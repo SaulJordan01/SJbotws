cda71652a6171deb5ae92ece9869d0e2b77287b0
FG98 <81941690+FG98F@users.noreply.github.com>
2021-10-28 05:01:10 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -481,6 +481,165 @@ number = '59172945992@s.whatsapp.net'
     Fg.fakeReply(from, text1, dia, text2, from)
     break
     
+    case 'pregunta:' 
+    if(!value) return m.reply(msg.notext)
+    naon = ["Si","No","En efecto"," Tal vez","No lo se","Quizas","2 dias","Jamas", "Un domingo", "no lo hace", "muy falso"]
+random = naon[Math.floor(Math.random() * (naon.length))]
+preg = `Pregunta : *${value}*
+Respuesta : ${random}`
+m.reply(preg)
+break
+
+case "s":
+  case "stiker":
+  case "sticker":
+    anu = args.join(" ").split("|");
+        a = anu[0] !== "" ? anu[0] : isAuthor;
+        b = typeof anu[1] !== "undefined" ? anu[1] : isPackname;
+    if ( 
+      ((isMedia && !mek.message.videoMessage) || isQuotedImage)
+      ) { 
+        const encmedia = isQuotedImage 
+        ? JSON.parse(JSON.stringify(mek).replace("quotedM", "m")).message .extendedTextMessage.contextInfo : mek;
+          media = await Fg.downloadAndSaveMediaMessage(encmedia); 
+          await createExif(a, b); 
+          out = getRandom(".webp"); 
+          ffmpeg(media) 
+          .on("error", (e) => { 
+            console.log(e); 
+            Fg.sendMessage(from, "Terjadi kesalahan", "conversation", { quoted: mek }); 
+            fs.unlinkSync(media); 
+            })
+            .on("end", () => { 
+              _out = getRandom(".webp"); 
+              spawn("webpmux", [
+                "-set",
+                "exif",
+                "./temp/data.exif",
+                out,
+                "-o",
+                _out,
+              ]).on("exit", () => {
+                Fg.sendMessage(
+                  from,
+                  fs.readFileSync(_out),
+                  "stickerMessage",
+                  { quoted: mek }
+                );
+                fs.unlinkSync(out);
+                fs.unlinkSync(_out);
+                fs.unlinkSync(media);
+              });
+            })
+            .addOutputOptions([
+              `-vcodec`,
+              `libwebp`,
+              `-vf`,
+              `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`,
+            ])
+            .toFormat("webp")
+            .save(out);
+        } else if (
+          ((isMedia && mek.message.videoMessage.seconds < 11) ||
+            (isQuotedVideo &&
+              mek.message.extendedTextMessage.contextInfo.quotedMessage
+                .videoMessage.seconds < 11))
+        ) {
+          const encmedia = isQuotedVideo
+            ? JSON.parse(JSON.stringify(mek).replace("quotedM", "m")).message
+                .extendedTextMessage.contextInfo
+            : mek;
+          const media = await Fg.downloadAndSaveMediaMessage(encmedia);
+          await createExif(a, b);
+          out = getRandom(".webp");
+          ffmpeg(media)
+            .on("error", (e) => {
+              console.log(e);
+              Fg.sendMessage(from, "Terjadi kesalahan", "conversation", {
+                quoted: mek,
+              });
+              fs.unlinkSync(media);
+            })
+            .on("end", () => {
+              _out = getRandom(".webp");
+              spawn("webpmux", [
+                "-set",
+                "exif",
+                "./temp/data.exif",
+                out,
+                "-o",
+                _out,
+              ]).on("exit", () => {
+                Fg.sendMessage(
+                  from,
+                  fs.readFileSync(_out),
+                  "stickerMessage",
+                  { quoted: mek }
+                );
+                fs.unlinkSync(out);
+                fs.unlinkSync(_out);
+                fs.unlinkSync(media);
+              });
+            })
+            .addOutputOptions([
+              `-vcodec`,
+              `libwebp`,
+              `-vf`,
+              `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`,
+            ])
+            .toFormat("webp")
+            .save(out);
+        } else {
+          m.reply(
+            `Kirim gambar dengan caption ${prefix}stiker, untuk video durasi kurang dari *7 detik*`
+          );
+        }
+        break
+
+  case "take":
+  case "robar":
+      if (!isQuotedSticker) return m.reply(msg.replyStic);
+      encmedia = JSON.parse(JSON.stringify(mek).replace("quotedM", "m"))
+        .message.extendedTextMessage.contextInfo;
+      media = await Fg.downloadAndSaveMediaMessage(encmedia);
+        anu = args.join(" ").split("|");
+        a = anu[0] !== "" ? anu[0] : isPackname;
+        b = typeof anu[1] !== "undefined" ? anu[1] : isAuthor;
+        createExif(a, b);
+        modStick(media, Fg, mek, from);
+        break 
+   
+  case "toimg":
+    if (!isQuotedSticker) return m.reply(msg.replyStic);
+        m.reply(msg.wait);
+        encmedia = JSON.parse(JSON.stringify(mek).replace("quotedM", "m"))
+          .message.extendedTextMessage.contextInfo;
+        media = await Fg.downloadAndSaveMediaMessage(encmedia);
+        ran = getRandom(".png");
+        exec(`ffmpeg -i ${media} ${ran}`, (err) => {
+          fs.unlinkSync(media);
+          if (err) return m.reply("⚠️ Error, inténtelo de nuevo ");
+          buffer = fs.readFileSync(ran);
+          Fg.sendMessage(from, buffer, image, {caption: msg.done})
+          fs.unlinkSync(ran);
+        });
+        break;
+        case 'joox':
+    if (!value) return m.reply(msg.notext)
+    m.reply(msg.wait) 
+    res = await fgx.joox(value)
+    let json = JSON.parse(JSON.stringify(res))
+    let hasil = json.data[0]
+    judul = hasil.lagu
+    artis = hasil.penyanyi
+    album = hasil.album
+    foto = hasil.img
+    lagu = hasil.mp3
+    jooxy = await getBuffer(foto)
+    path = await getBuffer(lagu)
+    Fg.adReply(from, path, document, judul+' - '+artis, artis+' ('+album+')', jooxy, 'https://www.instagram.com/p/CTKtDqeBgY5/?utm_medium=copy_link')
+  break
+    
 //---
   default:
   
