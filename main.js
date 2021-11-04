f1782644799a4fd398a47ec6b7d0f39c40b24d07
FG98 <81941690+FG98F@users.noreply.github.com>
2021-11-04 13:13:41 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const simple = require("./whatsapp/connecting.js");
 const WAConnection = simple.WAConnection(_WAConnection);
 const Fg = new WAConnection();
 const qrcode = require("qrcode-terminal");
-const {
+const { 
   cekWelcome,
   cekAntilink,
   cekBadword,
@@ -82,7 +82,7 @@ async function starts() {
         let tag = '@'+num.split('@')[0]
         let bye = await getCustomBye(mdata.id);
         
-        capt = bye.replace('@tag', tag).replace('@nama', username).replace('@about', about).replace('@tanggal', tanggal).replace('@group', mdata.subject);
+        capt = bye.replace('@user', tag).replace('@name', username).replace('@bio', about).replace('@date', tanggal).replace('@group', mdata.subject);
 	  //capt = `*Adios* @${num.split('@')[0]}`;
 	    Fg.sendMessage(mdata.id, capt, MessageType.text, { contextInfo: {"mentionedJid": [num]}});
       }
