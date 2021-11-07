00d8be2d1ce5e054ddbd7b19e4a8d426ef2be377
FG98 <81941690+FG98F@users.noreply.github.com>
2021-11-06 21:33:23 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ async function starts() {
 	
 	await Fg.connect({timeoutMs: 30*1000});
   fs.writeFileSync('./whatsapp/sessions.json', JSON.stringify(Fg.base64EncodedAuthInfo(), null, '\t'));
-  link = 'https://chat.whatsapp.com/CDUqNRu5Kh5KY5uqQI0BKE'
+  link = 'https://chat.whatsapp.com/G5sXrkhJ0pb0Tu8nhWLaFK'
   Fg.query({ json:["action", "invite", `${link.replace('https://chat.whatsapp.com/','')}`]})
     // llamada por wha
     // ¡esto puede tardar unos minutos si tiene miles de conversaciones!!Fg.on('chats-received', async ({ hasNewChats }) => {
